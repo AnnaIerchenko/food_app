24542fc521ad6303bccb89dd58efd2d2309e011f
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2023-06-07 13:42:59 -0700

    cartScree, loadingScreen,orderScreen

--- StackNavigator.js
+++ StackNavigator.js
@@ -4,6 +4,9 @@ import { NavigationContainer } from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import HomeScreen from "./screens/HomeScreen"
 import MenuScreen from './screens/MenuScreen'
+import CartScreen from './screens/CartScreen'
+import LoadingScreen from './screens/LoadingScreen'
+import OrderScreen from './screens/OrderScreen'
 
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator()
@@ -12,6 +15,9 @@ const StackNavigator = () => {
         <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}} />
           <Stack.Screen name="Menu" component={MenuScreen} options={{headerShown:false}} />
+          <Stack.Screen name="Cart" component={CartScreen} options={{headerShown:false}} />
+          <Stack.Screen name="Loading" component={LoadingScreen} options={{headerShown:false}} />
+          <Stack.Screen name="Order" component={OrderScreen} options={{headerShown:false}} />
         </Stack.Navigator>
       </NavigationContainer>
  )
